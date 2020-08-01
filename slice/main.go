19cdd25bcf19635fde24f3349423d2ae402e40a9
mazrean <shunsuke.w04@gmail.com>
2020-08-02 07:23:15 +0900

    sliceテスト

---
+++ slice/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	num := 10000000
+	times := 5
+
+	start := time.Now()
+	for i := 0;i < num;i++ {
+		s := []int{}
+		for j := 0;j < times;j++ {
+			s = append(s, j)
+		}
+	}
+	end := time.Now()
+	fmt.Println("no cap:", end.Sub(start).Milliseconds(), "ms")
+
+	start = time.Now()
+	for i := 0;i < num;i++ {
+		s := make([]int, 0, times)
+		for j := 0;j < times;j++ {
+			s = append(s, j)
+		}
+	}
+	end = time.Now()
+	fmt.Println("with cap:", end.Sub(start).Milliseconds(), "ms")
+}
