c3abe487a011aba7d88720ef893a5e93ce1dc486
mazrean <shunsuke.w04@gmail.com>
2020-08-02 13:29:01 +0900

    errorの例追加

---
+++ error1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func div(x int, y int) (int,error) {
+	if y == 0 {
+		return 0, errors.New("zero div") //yが0のときにエラーを返す
+	}
+
+	return x/y, nil //エラーがでなければnilを返す
+}
+
+func main() {
+	result, err := div(2,1)
+	if err != nil {
+		panic(err) // errがnil出ない場合、ここでpanic
+	}
+
+	fmt.Println(result)
+
+	result, err = div(2, 0)
+	if err != nil {
+		panic(err)
+	}
+}
