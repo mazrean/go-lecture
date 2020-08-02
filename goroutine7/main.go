fbbb5bfcb25e1aa45a2f1b2a8c2264b511ab2cd2
mazrean <shunsuke.w04@gmail.com>
2020-08-02 18:08:12 +0900

    goroutine7

---
+++ goroutine7/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func despair(ctx context.Context) {
+	innerCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	for {
+		fmt.Println("Help me!")
+
+        select {
+        case <- innerCtx.Done():
+            fmt.Println("...")
+            return
+        }
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*1)
+	defer cancel()
+
+	go despair(ctx)
+
+	select {
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+	}
+}
