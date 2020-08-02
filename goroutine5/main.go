5564afaca83f00252e135d1c1ba6061efebc086d
mazrean <shunsuke.w04@gmail.com>
2020-08-02 17:52:40 +0900

    goroutine5

---
+++ goroutine5/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func despair(ctx context.Context) {
+	for {
+		fmt.Println("Help me!")
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	go despair(ctx)
+
+	time.Sleep(time.Second*1)
+	cancel()
+
+
+	select {
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+		time.Sleep(time.Millisecond*100)
+	}
+}
